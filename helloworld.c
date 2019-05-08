6d25b9b5da42b886f2c6f87157d223b6ccf6e843
Devilal <Devd2101@users.noreply.github.com>
2019-05-08 12:48:27 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 
 int main(){
-  char data[50]={0};
-  int a , b;
-  printf("Hello world!!!\nThis is my first program!!\n");
-  printf("Enter your name\n");
-  scanf("%s", data);
-  printf("you entered : %s\n",data);
   
-  printf("Enter number for addition\n");
-  scanf("%d %d",&a, &b);
- 
-  printf("sum of %d and %d is : %d\n", a, b, (a+b));
-  //EXIT(0);
+  printf("Hello world!!!\n");
   return 0;
 }
