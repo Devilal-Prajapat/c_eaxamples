d76c89ebf9d1658f000a1a2a9d278ddc84f27263
Devilal Prajapat <13392395+Devilal-Prajapat@users.noreply.github.com>
2022-06-13 18:48:58 +0530

    Create file_handling2.cpp

---
+++ file_handling2.cpp
@@ -0,0 +1,72 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+class Student
+{
+    public:
+    string name;
+    int rollno;
+    string branch;
+    friend ofstream & operator<<(ofstream &ofs, Student &s);
+    friend ifstream & operator>>(ifstream &ifs, Student &s);
+    
+};
+
+ofstream & operator<<(ofstream &ofs, Student &s)
+{
+    ofs<<s.name<<endl;
+    ofs<<s.rollno<<endl;
+    ofs<<s.branch<<endl;
+    return ofs;
+}
+
+ifstream & operator>>(ifstream &ifs, Student &s)
+{
+    ifs>>s.name;
+    ifs>>s.rollno;
+    ifs>>s.branch;
+    return ifs;
+}
+
+int main()
+{
+    // ofstream outfile("demo.txt");
+    // outfile<<"hello"<<endl;
+    // outfile<<123<<endl;
+    // outfile.close();
+    Student s1;
+    s1.name = "Dev";
+    s1.rollno = 10;
+    s1.branch = "EC";
+    ofstream ofs("demo.txt");
+    ofs<<s1;
+    ofs<<s1; 
+    ofs<<s1;
+    ofs.close();
+    
+    Student s2;
+    ifstream ifs;
+    ifs.open("demo.txt");
+    ifs>>s2;
+    cout<<s2.name<<" "<<s2.rollno<<" "<<s2.branch<<endl;
+    ifs>>s2;
+    cout<<s2.name<<" "<<s2.rollno<<" "<<s2.branch<<endl;
+    ifs>>s2;
+    cout<<s2.name<<" "<<s2.rollno<<" "<<s2.branch<<endl;
+    ifs>>s2;
+    cout<<s2.name<<" "<<s2.rollno<<" "<<s2.branch<<endl;
+    if(ifs.eof())
+    {
+        cout<<"eof reached"<<endl;
+    }
+     Student s3;
+    ifs>>s3;
+    cout<<s3.name<<" "<<s3.rollno<<" "<<s3.branch<<endl;
+
+    
+    ifs.close();
+}
